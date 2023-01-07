3010fda9e4d0ad5476eac10a1d74fbb7c4798ed8
Mads Hartmann <mads379@gmail.com>
2023-01-07 15:52:51 +0000

    Use Inter font

--- sites/howto.mads-hartmann.com/pages/_app.tsx
+++ sites/howto.mads-hartmann.com/pages/_app.tsx
@@ -1,5 +1,12 @@
 import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={inter.className}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
