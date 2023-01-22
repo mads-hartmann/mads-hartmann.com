53b59176cf46b7a0af26f6e21baea459f48ca5ef
Mads Hartmann <mads379@gmail.com>
2023-01-22 16:04:09 +0000

    WIP

--- sites/blog.mads-hartmann.com/devenv.nix
+++ sites/blog.mads-hartmann.com/devenv.nix
@@ -5,13 +5,14 @@
   #       bundle install
   # Haven't figured out why yet.
   packages = [
+    pkgs.ruby_3_1
     pkgs.openssl_1_1
     pkgs.zlib
     pkgs.gnumake
     pkgs.gcc
   ];
-  languages.nix.enable = true;
-  languages.ruby.enable = true;
+  # languages.nix.enable = true;
+  # languages.ruby.enable = true;
   processes = {
     serve.exec = "bundle install && bundle exec jekyll serve --watch --drafts --source src";
   };
