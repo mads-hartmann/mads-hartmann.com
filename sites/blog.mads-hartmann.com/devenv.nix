744db982c3bdfe8c908bba415329611f79a90b46
Mads Hartmann <mads379@gmail.com>
2023-01-23 20:55:02 +0000

    WIP

--- sites/blog.mads-hartmann.com/devenv.nix
+++ sites/blog.mads-hartmann.com/devenv.nix
@@ -3,6 +3,21 @@ let
   gems = pkgs.bundlerEnv {
     name = "gems-for-blog";
     gemdir = ./.;
+    ruby = pkgs.ruby_3_1;
+    # This was a hard problem to figure out.
+    # cacert is needed for bundix
+    # For more context, see https://github.com/NixOS/nixpkgs/issues/66716#issuecomment-883399373
+    # pkgs.cacert
+    # Well that didn't fix it. Maybe I need to somehow tell Nix to use
+    # export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+    # but I have no idea how
+
+    gemConfig = pkgs.defaultGemConfig // {
+      sass-embedded = attrs: {
+        # nativeBuildInputs = [ pkgs.cacert ];
+        buildInputs = [ pkgs.cacert ];
+      };
+    };
   };
 in 
 {
@@ -10,7 +25,10 @@ in
     pkgs.bundix
     gems
   ];
-  languages.ruby.enable = true;
+  languages.ruby = {
+    package = pkgs.ruby_3_1;
+    enable = true;
+  };
   processes = {
     serve.exec = "jekyll serve --watch --drafts --source src";
   };
