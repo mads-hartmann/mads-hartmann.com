ca9643708028ae28b04be0c65372d0abbcb0a847
Mads Hartmann <mads379@gmail.com>
2023-12-10 19:27:15 +0000

    WIP

--- sites/computer.mads-hartmann.com/index.js
+++ sites/computer.mads-hartmann.com/index.js
@@ -1,3 +1,30 @@
 import ejs from "ejs";
+import fs from "fs";
+import path from "path";
+import { marked } from "marked";
 
-const template = ejs.compile("");
+async function main() {
+  const srcDir = "src";
+  const pagesDir = `${srcDir}/pages`;
+
+  const layoutPath = `${srcDir}/layout.ejs`;
+  const layoutContent = await fs.promises.readFile(layoutPath, "utf8");
+  const template = ejs.compile(layoutContent);
+
+  const ps = await fs.promises.readdir(pagesDir);
+  const pss = ps.map(async (filename) => {
+    const filePath = path.join(pagesDir, filename);
+    const markdown = await fs.promises.readFile(filePath, "utf8");
+    const h = await marked.parse(markdown);
+    const data = {
+      title: "test",
+      content: h,
+    };
+    const html = template(data);
+    console.log(html);
+    return html;
+  });
+  await Promise.all(pss);
+}
+
+await main();
